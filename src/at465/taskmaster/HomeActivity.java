84750aee3c080d67936d891a1b24479086119671
Antony Tran <antonyt@gmail.com>
2011-09-25 15:18:17 +0100

    Use viewpager to hold the fragments.

--- src/at465/taskmaster/HomeActivity.java
+++ src/at465/taskmaster/HomeActivity.java
@@ -1,27 +1,42 @@
 package at465.taskmaster;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import at465.taskmaster.fragment.TasksListFragment;
 
 public class HomeActivity extends FragmentActivity {
 
     private TasksListFragment list;
-    
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.main);
 
-	if (savedInstanceState == null) {
-	    list = new TasksListFragment();
-	    Bundle args = new Bundle();
-	    args.putString("taskListId", "@default");
-	    list.setArguments(args);
-	    getSupportFragmentManager().beginTransaction().add(R.id.list_container, list, TasksListFragment.class.getName()).commit();
-	} else {
-	    list = (TasksListFragment) getSupportFragmentManager().findFragmentByTag(TasksListFragment.class.getName());
-	}
+	ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+	viewPager.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+
+	    @Override
+	    public int getCount() {
+		return 1;
+	    }
+
+	    @Override
+	    public Fragment getItem(int position) {
+		list = new TasksListFragment();
+		Bundle args = new Bundle();
+		switch (position) {
+		case 0:
+		    args.putString("taskListId", "@default");
+		    break;
+		}
+		list.setArguments(args);
+		return list;
+	    }
+	});
 
     }
 
